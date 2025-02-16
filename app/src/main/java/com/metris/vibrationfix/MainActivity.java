03aefcf9ceb113e0ee9c300707d0298c6200f682
Massi-X <vnnmax@yahoo.it>
2025-02-16 13:05:21 +0100

    Update strings.xml and MainActivity.java - Add explanation for restricted settings - Make it clear it is not needed for Android 15+ - Switch buttons around and add one for opening app setting page

--- app/src/main/java/com/metris/vibrationfix/MainActivity.java
+++ app/src/main/java/com/metris/vibrationfix/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -25,14 +26,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //hide all the graphics because we use the dialog only
-        setTitle("");
-        overridePendingTransition(0, 0);
+        //override launcher default open/close animation (>= 34+ default is fine)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+            overridePendingTransition(0, 0);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.Theme_Dialog)
                 .setTitle(R.string.dialog_title)
                 .setMessage(R.string.info_text)
-                .setNeutralButton(R.string.accessibility_page, (dialog, which) -> {
+                .setNeutralButton(R.string.hide_app, (dialog, which) -> {
+                    Toast.makeText(this, R.string.goodbye, Toast.LENGTH_LONG).show();
+                    //hide in the standard Android way (supported on Android 10+ because no permissions)
+                    MainActivity.this.getPackageManager().setComponentEnabledSetting(
+                            new ComponentName(BuildConfig.APPLICATION_ID, BuildConfig.APPLICATION_ID + ".MainAlias"),
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+                })
+                .setNegativeButton(R.string.accessibility_page, (dialog, which) -> {
                     Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                     try {
                         startActivity(intent);
@@ -40,15 +48,13 @@ public class MainActivity extends Activity {
                         Toast.makeText(this, R.string.activity_not_found, Toast.LENGTH_LONG).show();
                     }
                 })
-                .setNegativeButton(R.string.hide_app, (dialog, which) -> {
-                    Toast.makeText(this, R.string.goodbye, Toast.LENGTH_LONG).show();
-                    //hide in the standard Android way (supported on Android 10+ because no permissions)
-                    MainActivity.this.getPackageManager().setComponentEnabledSetting(
-                            new ComponentName(BuildConfig.APPLICATION_ID, BuildConfig.APPLICATION_ID + ".MainAlias"),
-                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+                .setPositiveButton(R.string.app_settings_page, (dialog, which) -> {
+                    try {
+                        startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(this, R.string.activity_not_found, Toast.LENGTH_LONG).show();
+                    }
                 })
-                .setPositiveButton(R.string.support_me, (dialog, which) ->
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_store)))))
                 .setOnDismissListener((dialog) -> {
                     if (!dismissForConfigChange)
                         new Handler(Objects.requireNonNull(Looper.myLooper())).postDelayed(MainActivity.this::finish, 250);
